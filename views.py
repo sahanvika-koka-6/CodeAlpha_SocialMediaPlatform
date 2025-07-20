7c02cb1c9dbe21ca8acf2bcc7ccdbf6979158fb2
Sahanvika <sahanvikakoka12@gmail.com>
2025-07-20 20:33:43 +0530

    Files
    Main

---
+++ views.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
